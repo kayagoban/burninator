7668565d0f49a6efd713b62de5a59eca47a6f02d
C Thomas <cthomas@soykaf.digital>
2019-06-06 22:42:45 -0500

    Final version of burninator app

--- __init__.py
+++ __init__.py
@@ -2,19 +2,44 @@ from shadowlands.sl_dapp import SLDapp
 from shadowlands.sl_frame import SLFrame
 from burninator.peasant_coin import PeasantCoin
 from decimal import Decimal
+from cached_property import cached_property
 from shadowlands.tui.debug import debug, end_debug
 import pdb
 
 class Dapp(SLDapp):
     def initialize(self):
         self.token = PeasantCoin(self.node)
-        self.peasants = Decimal(self.token.my_balance() / (10 ** 18))
-        self.total_peasants =  self.token.totalSupply() / (10 ** 18)
-        self.my_burninated_peasants = self.token.burninatedBy(self.node.credstick.address) / (10 ** 18)
         self.add_sl_frame(MyMenuFrame(self, height=24, width=74 ))
+
+        self.victory_notification_has_been_seen = False
+        self.victory_check()
+
+    def victory_check(self):
+        if self.victory_notification_has_been_seen:
+            return
 
         if self.token.victorious():
             self.add_sl_frame(VictoryFrame(self, height=9, width=62, title="Victory!!!"))
+            self.victory_notification_has_been_seen = True
+
+
+    def new_block_callback(self):
+        self.victory_check()
+
+    @cached_property
+    def total_peasants(self):
+        return self.token.totalSupply() / (10 ** 18)
+
+    @cached_property
+    def my_burninated_peasants(self):
+        return self.token.burninatedBy(self.node.credstick.address) / (10 ** 18)
+
+    @cached_property
+    def peasants(self):
+        return Decimal(self.token.my_balance() / (10 ** 18))
+
+    def peasant_decorator(self, peasants):
+        return "{:f}".format(peasants)[:14]
 
 
 class MyMenuFrame(SLFrame):
@@ -23,13 +48,13 @@ class MyMenuFrame(SLFrame):
         self.add_divider(draw_line=True)
         self.add_label("Rank    Peasants           Hero", add_divider=False)
 
-        for heroes in self.top_burninators_decorator():
-            self.add_label(heroes, add_divider=False)
+        for i in range(10):
+            self.add_label(self.burninator_hero(i), add_divider=False)
         self.add_divider(draw_line=True)
 
         self.add_label("Trogdor the wingaling dragon intends to burninate peasants.", add_divider=False)
-        self.add_label("There are {} peasants (BRNT) in the world.".format(self.peasant_decorator(self.dapp.total_peasants)))
-        self.add_label("Trogdor has {} peasants, and has burninated {}".format(self.peasant_decorator(self.dapp.peasants), self.peasant_decorator(self.dapp.my_burninated_peasants)))
+        self.add_label(lambda: self.total_peasants_string)
+        self.add_label(lambda: self.my_peasant_status_string)
         self.text_value = self.add_textbox("How many to burninate?", default_value=' ')
         self.add_button_row([
             ("Burninate!", self.burninate, 0),
@@ -38,18 +63,28 @@ class MyMenuFrame(SLFrame):
         ], layout=[30, 40, 30]
         )
 
-        
+    @cached_property
+    def total_peasants_string(self):
+        return "There are {} peasants (BRNT) in the world.".format(self.dapp.peasant_decorator(self.dapp.total_peasants))
+
+    @cached_property
+    def my_peasant_status_string(self):
+        return "Trogdor has {} peasants, and has burninated {}".format(self.dapp.peasant_decorator(self.dapp.peasants), self.dapp.peasant_decorator(self.dapp.my_burninated_peasants))
+
+    def burninator_hero(self, index):
+        return lambda: self.top_burninators_decorator[index]
+
+    @cached_property
     def top_burninators_decorator(self):
         burninators = self.dapp.token.top_burninators()
         i = 0 
         heroes = []
 
-        #debug(); pdb.set_trace()
         for hero in burninators:
             hero_name = self.dapp.node._ns.name(hero[0])
             if hero_name is None:
                 hero_name = hero[0]
-            heroes.append("{}       {:14s}     {}".format(i, self.peasant_decorator(hero[1]), hero_name))
+            heroes.append("{}       {:14s}     {}".format(i, self.dapp.peasant_decorator(hero[1]), hero_name))
             i += 1 
 
         if len(heroes) < 10:
@@ -58,9 +93,6 @@ class MyMenuFrame(SLFrame):
                     "{}       Unclaimed".format(str(x)))
 
         return heroes
-
-    def peasant_decorator(self, peasants):
-        return "{:f}".format(peasants)[:14]
 
 
     def get_peasants(self):
@@ -83,29 +115,25 @@ class MyMenuFrame(SLFrame):
         return True
 
 
-
     def burninate(self):
         if not self.peasants_validated():
             return
 
         tokens_to_burninate = self.peasants_to_burninate * Decimal(10 ** 18)
-
         burn_fn = self.dapp.token.burninate(
             int(tokens_to_burninate)
         )
-
         self.dapp.add_transaction_dialog(
             burn_fn, 
             title="Trogdor burninates the tokens", 
             gas_limit=56000
         )
 
-        self.close()
 
 class VictoryFrame(SLFrame):
     def initialize(self):
         self.add_label("Congratulations!  You have racked up a truly impressive", add_divider=False)
-        self.add_label("count of {} burninated peasants, as well".format(self.peasant_decorator(self.dapp.my_burninated_peasants)), add_divider=False)
+        self.add_label("count of {} burninated peasants, as well".format(self.dapp.peasant_decorator(self.dapp.my_burninated_peasants)), add_divider=False)
         self.add_label("as several incinerated thatched roof cottages and various", add_divider=False)
         self.add_label("counts of petty theft and vandalism.  Your throne in the", add_divider=False)
         self.add_label("Hall of Maximum Burnination awaits your Dragonly Personage!")
@@ -123,6 +151,4 @@ class VictoryFrame(SLFrame):
         )
         self.close()
 
-    def peasant_decorator(self, peasants):
-        return "{:f}".format(peasants)[:14]
 
